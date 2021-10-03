7b245795204342826d1e432e3006b242f35fa031
LuisPulenta <luisalbertonu@gmail.com>
2021-10-03 11:14:08 -0300

    Clase NPresentacion

---
+++ CapaDatos/DPresentacion.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class DPresentacion
+    {
+        private int _IdPresentacion;
+        public int IdPresentacion { get => _IdPresentacion; set => _IdPresentacion = value; }
+
+        private string _Nombre;
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+
+        private string _Descripcion;
+        public string Descripcion { get => _Descripcion; set => _Descripcion = value; }
+
+        private string _TextoBuscar;
+        public string TextoBuscar { get => _TextoBuscar; set => _TextoBuscar = value; }
+
+        //Constructor vacío
+        public DPresentacion()
+        {
+
+        }
+
+        //Constructor
+        public DPresentacion(int idpresentacion, string nombre, string descripcion, string textbuscar)
+        {
+            IdPresentacion = idpresentacion;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            TextoBuscar = textbuscar;
+        }
+
+        //Método Insertar
+        public string Insertar(DPresentacion Presentacion)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Establecer la conexión
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer el Comando
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "sppresentacion_insertar";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdpresentacion = new SqlParameter();
+                ParIdpresentacion.ParameterName = "@idpresentacion";
+                ParIdpresentacion.SqlDbType = SqlDbType.Int;
+                ParIdpresentacion.Direction = ParameterDirection.Output;
+                SqlCmd.Parameters.Add(ParIdpresentacion);
+
+                SqlParameter ParNombre = new SqlParameter();
+                ParNombre.ParameterName = "@nombre";
+                ParNombre.SqlDbType = SqlDbType.VarChar;
+                ParNombre.Size = 50;
+                ParNombre.Value = Presentacion.Nombre;
+                SqlCmd.Parameters.Add(ParNombre);
+
+                SqlParameter ParDescripcion = new SqlParameter();
+                ParDescripcion.ParameterName = "@descripcion";
+                ParDescripcion.SqlDbType = SqlDbType.VarChar;
+                ParDescripcion.Size = 256;
+                ParDescripcion.Value = Presentacion.Descripcion;
+                SqlCmd.Parameters.Add(ParDescripcion);
+
+                //Ejecutar comando
+                rpta = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "No se ingresó el Registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+        //Método Editar
+        public string Editar(DPresentacion Presentacion)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Establecer la conexión
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer el Comando
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "sppresentacion_editar";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdpresentacion = new SqlParameter();
+                ParIdpresentacion.ParameterName = "@idpresentacion";
+                ParIdpresentacion.SqlDbType = SqlDbType.Int;
+                ParIdpresentacion.Value = Presentacion.IdPresentacion;
+                SqlCmd.Parameters.Add(ParIdpresentacion);
+
+                SqlParameter ParNombre = new SqlParameter();
+                ParNombre.ParameterName = "@nombre";
+                ParNombre.SqlDbType = SqlDbType.VarChar;
+                ParNombre.Size = 50;
+                ParNombre.Value = Presentacion.Nombre;
+                SqlCmd.Parameters.Add(ParNombre);
+
+                SqlParameter ParDescripcion = new SqlParameter();
+                ParDescripcion.ParameterName = "@descripcion";
+                ParDescripcion.SqlDbType = SqlDbType.VarChar;
+                ParDescripcion.Size = 256;
+                ParDescripcion.Value = Presentacion.Descripcion;
+                SqlCmd.Parameters.Add(ParDescripcion);
+
+                //Ejecutar comando
+                rpta = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "No se actualizó el Registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+        //Método Eliminar
+        public string Eliminar(DPresentacion Presentacion)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Establecer la conexión
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer el Comando
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "sppresentacion_eliminar";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdpresentacion = new SqlParameter();
+                ParIdpresentacion.ParameterName = "@idpresentacion";
+                ParIdpresentacion.SqlDbType = SqlDbType.Int;
+                ParIdpresentacion.Value = Presentacion.IdPresentacion;
+                SqlCmd.Parameters.Add(ParIdpresentacion);
+
+                //Ejecutar comando
+                rpta = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "No se eliminó el Registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+        //Método Mostrar
+        public DataTable Mostrar()
+        {
+            DataTable DtResultado = new DataTable("presentacion");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Establecer la conexión
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer el Comando
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "sppresentacion_mostrar";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                //Ejecutar comando
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return DtResultado;
+        }
+
+        //Método BuscarNombre
+        public DataTable BuscarNombre(DPresentacion Presentacion)
+        {
+            DataTable DtResultado = new DataTable("presentacion");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Establecer la conexión
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer el Comando
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "sppresentacion_buscar";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParTextoBuscar = new SqlParameter();
+                ParTextoBuscar.ParameterName = "@textobuscar";
+                ParTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar.Size = 50;
+                ParTextoBuscar.Value = Presentacion.TextoBuscar;
+                SqlCmd.Parameters.Add(ParTextoBuscar);
+
+                //Ejecutar comando
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return DtResultado;
+        }
+    }
+}
---
+++ CapaNegocios/NPresentacion.cs
@@ -0,0 +1,49 @@
+﻿using CapaDatos;
+using System.Data;
+
+namespace CapaNegocios
+{
+    public class NPresentacion
+    {
+        //Método Insertar
+        public static string Insertar(string nombre, string descripcion)
+        {
+            DPresentacion Obj = new DPresentacion();
+            Obj.Nombre = nombre;
+            Obj.Descripcion = descripcion;
+            return Obj.Insertar(Obj);
+        }
+
+        //Método Editar
+        public static string Editar(int idpresentacion, string nombre, string descripcion)
+        {
+            DPresentacion Obj = new DPresentacion();
+            Obj.IdPresentacion = idpresentacion;
+            Obj.Nombre = nombre;
+            Obj.Descripcion = descripcion;
+            return Obj.Editar(Obj);
+        }
+
+        //Método Eliminar
+        public static string Eliminar(int idpresentacion)
+        {
+            DPresentacion Obj = new DPresentacion();
+            Obj.IdPresentacion = idpresentacion;
+            return Obj.Eliminar(Obj);
+        }
+
+        //Método Mostrar
+        public static DataTable Mostrar()
+        {
+            return new DPresentacion().Mostrar();
+        }
+
+        //Método Buscar
+        public static DataTable BuscarNombre(string textobuscar)
+        {
+            DPresentacion Obj = new DPresentacion();
+            Obj.TextoBuscar = textobuscar;
+            return Obj.BuscarNombre(Obj);
+        }
+    }
+}
