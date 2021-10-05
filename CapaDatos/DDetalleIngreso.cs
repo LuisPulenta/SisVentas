a49320a1f325b44984953c8c1a97a7f514a44aee
LuisPulenta <luisalbertonu@gmail.com>
2021-10-05 19:27:58 -0300

    DDetalleIngreso a medias

---
+++ CapaDatos/DDetalleIngreso.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class DDetalleIngreso
+    {
+        private int _iddetalle_ingreso;
+        public int Iddetalle_ingreso { get => _iddetalle_ingreso; set => _iddetalle_ingreso = value; }
+
+        private int _idingreso;
+        public int Idingreso { get => _idingreso; set => _idingreso = value; }
+
+        private int _idarticulo;
+        public int Idarticulo { get => _idarticulo; set => _idarticulo = value; }
+
+        private decimal _precio_compra;
+        public decimal Precio_compra { get => _precio_compra; set => _precio_compra = value; }
+
+        private decimal _precio_venta;
+        public decimal Precio_venta { get => _precio_venta; set => _precio_venta = value; }
+
+        private int _stock_inicial;
+        public int Stock_inicial { get => _stock_inicial; set => _stock_inicial = value; }
+
+        private int _stock_actual;
+        public int Stock_actual { get => _stock_actual; set => _stock_actual = value; }
+
+        private DateTime _fecha_produccion;
+        public DateTime Fecha_produccion { get => _fecha_produccion; set => _fecha_produccion = value; }
+
+        private DateTime _fecha_vencimiento;
+        public DateTime Fecha_vencimiento { get => _fecha_vencimiento; set => _fecha_vencimiento = value; }
+
+        //Constructor vacío
+        public DDetalleIngreso()
+        {
+
+        }
+
+        //Constructor
+        public DDetalleIngreso(int iddetalle_ingreso, int idingreso, int idarticulo, decimal precio_compra, decimal precio_venta, int stock_inicial, int stock_actual, DateTime fecha_produccion, DateTime fecha_vencimiento)
+        {
+            Iddetalle_ingreso = iddetalle_ingreso;
+            Idingreso = idingreso;
+            Idarticulo = idarticulo;
+            Precio_compra = precio_compra;
+            Precio_venta = precio_venta;
+            Stock_inicial = stock_inicial;
+            Stock_actual = stock_actual;
+            Fecha_produccion = fecha_produccion;
+            Fecha_vencimiento = fecha_vencimiento;
+        }
+
+        //Método Insertar
+        public string Insertar(DCategoria Categoria)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Establecer la conexión
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer el Comando
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spcategoria_insertar";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdcategoria = new SqlParameter();
+                ParIdcategoria.ParameterName = "@idcategoria";
+                ParIdcategoria.SqlDbType = SqlDbType.Int;
+                ParIdcategoria.Direction = ParameterDirection.Output;
+                SqlCmd.Parameters.Add(ParIdcategoria);
+
+                SqlParameter ParNombre = new SqlParameter();
+                ParNombre.ParameterName = "@nombre";
+                ParNombre.SqlDbType = SqlDbType.VarChar;
+                ParNombre.Size = 50;
+                ParNombre.Value = Categoria.Nombre;
+                SqlCmd.Parameters.Add(ParNombre);
+
+                SqlParameter ParDescripcion = new SqlParameter();
+                ParDescripcion.ParameterName = "@descripcion";
+                ParDescripcion.SqlDbType = SqlDbType.VarChar;
+                ParDescripcion.Size = 256;
+                ParDescripcion.Value = Categoria.Descripcion;
+                SqlCmd.Parameters.Add(ParDescripcion);
+
+                //Ejecutar comando
+                rpta = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "No se ingresó el Registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+        //Método Editar
+        public string Editar(DCategoria Categoria)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Establecer la conexión
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer el Comando
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spcategoria_editar";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdcategoria = new SqlParameter();
+                ParIdcategoria.ParameterName = "@idcategoria";
+                ParIdcategoria.SqlDbType = SqlDbType.Int;
+                ParIdcategoria.Value = Categoria.IdCategoria;
+                SqlCmd.Parameters.Add(ParIdcategoria);
+
+                SqlParameter ParNombre = new SqlParameter();
+                ParNombre.ParameterName = "@nombre";
+                ParNombre.SqlDbType = SqlDbType.VarChar;
+                ParNombre.Size = 50;
+                ParNombre.Value = Categoria.Nombre;
+                SqlCmd.Parameters.Add(ParNombre);
+
+                SqlParameter ParDescripcion = new SqlParameter();
+                ParDescripcion.ParameterName = "@descripcion";
+                ParDescripcion.SqlDbType = SqlDbType.VarChar;
+                ParDescripcion.Size = 256;
+                ParDescripcion.Value = Categoria.Descripcion;
+                SqlCmd.Parameters.Add(ParDescripcion);
+
+                //Ejecutar comando
+                rpta = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "No se actualizó el Registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+        //Método Eliminar
+        public string Eliminar(DCategoria Categoria)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Establecer la conexión
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer el Comando
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spcategoria_eliminar";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdcategoria = new SqlParameter();
+                ParIdcategoria.ParameterName = "@idcategoria";
+                ParIdcategoria.SqlDbType = SqlDbType.Int;
+                ParIdcategoria.Value = Categoria.IdCategoria;
+                SqlCmd.Parameters.Add(ParIdcategoria);
+
+                //Ejecutar comando
+                rpta = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "No se eliminó el Registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+        //Método Mostrar
+        public DataTable Mostrar()
+        {
+            DataTable DtResultado = new DataTable("categoria");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Establecer la conexión
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer el Comando
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spcategoria_mostrar";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                //Ejecutar comando
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return DtResultado;
+        }
+
+        //Método BuscarNombre
+        public DataTable BuscarNombre(DCategoria Categoria)
+        {
+            DataTable DtResultado = new DataTable("categoria");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Establecer la conexión
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer el Comando
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spcategoria_buscar";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParTextoBuscar = new SqlParameter();
+                ParTextoBuscar.ParameterName = "@textobuscar";
+                ParTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar.Size = 50;
+                ParTextoBuscar.Value = Categoria.TextoBuscar;
+                SqlCmd.Parameters.Add(ParTextoBuscar);
+
+                //Ejecutar comando
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return DtResultado;
+        }
+    }
+}
